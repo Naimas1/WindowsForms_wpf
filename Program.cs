5ca69db0ab988649ad2199656a34c52c5cf18473
Denis Riabchuk <Naimas@DESKTOP-68Q4MHG>
2023-10-03 19:15:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsForms_wpf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
